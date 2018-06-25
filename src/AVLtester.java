46ee7950b52812565b3de709dd02583581712332
Steven Golob <golobs@uw.edu>
2018-06-24 23:59:52 -0700

    Modified main test

--- src/AVLtester.java
+++ src/AVLtester.java
@@ -7,27 +7,6 @@ public class AVLtester {
     public static void main(String[] args) {
         AVLtree<Integer> avl = new AVLtree<>();
         
-        ReplaceableStack<NotComparableObject> s = new ReplaceableStack<>();
-
-        NotComparableObject replaced = new NotComparableObject(4);
-
-        s.push(replaced);
-        
-        s.push(new NotComparableObject(5));
-        s.push(new NotComparableObject(6));
-        s.push(new NotComparableObject(7));
-        s.push(new NotComparableObject(8));
-        s.push(new NotComparableObject(9));
-        s.push(new NotComparableObject(10));
-        
-        s.replaceHighestInstance(new NotComparableObject(12), new NotComparableObject(15));
-        
-        while (!s.isEmpty()) {
-            System.out.print(s.pop().toString());
-        }
-        
-        
-        
         avl.insert(25);
         avl.insert(20);
         avl.insert(30);
@@ -45,22 +24,22 @@ public class AVLtester {
         
         avl.remove(25);
         avl.remove(26);
-        avl.remove(17);
-        avl.remove(16);
-        avl.remove(5);
-        Random rand = new Random();
         
-        for (int i = 0; i <= 100; i++) {
-            avl.insert(rand.nextInt(5000));
-        }
-
-        for (int i = 0; i <= 50000; i++) {
-            avl.remove(rand.nextInt(5000));
-        }
-
         System.out.println(avl.toString());
-
         System.out.println(avl.size());
+        
+        
+//        Random rand = new Random();
+//        
+//        for (int i = 0; i <= 100; i++) {
+//            avl.insert(rand.nextInt(5000));
+//        }
+//        for (int i = 0; i <= 50000; i++) {
+//            avl.remove(rand.nextInt(5000));
+//        }
+//
+//        System.out.println(avl.toString());
+//        System.out.println(avl.size());
     }
 
 }
